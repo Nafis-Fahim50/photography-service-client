17867e628982042ddf372061f2c5440e1221ea14
Nafis-Fahim50 <nafisf96@gmail.com>
2022-12-21 11:49:45 +0600

    resize pic

--- src/Pages/Home/Banner/BannerItem.js
+++ src/Pages/Home/Banner/BannerItem.js
@@ -16,7 +16,7 @@ const BannerItem = ({ slider }) => {
                 <p className='text-white mt-24 font-semibold'>Hey there! I'm Nafis Fahim</p>
             </div>
             <div className="absolute flex justify-end transform -translate-y-1/2 left-24 mt-5 lg:mt-10 top-1/4">
-                <p className='lg:text-4xl mt-24 font-extrabold text-rose-500'>Wedding & Event Photographer</p>
+                <p className='lg:text-4xl mt-24 font-extrabold text-orange-500'>Wedding & Event Photographer</p>
             </div>
             <div className="hidden lg:block absolute flex justify-end transform -translate-y-1/2 w-2/5 left-24  mt-12 lg:mt-24 top-1/4">
                 <p className='text-white mt-24 font-semibold'>If you are looking for best wedding photography service? Then here you can find different type of wedding photography services.
@@ -24,7 +24,7 @@ const BannerItem = ({ slider }) => {
             </div>
             <div className="absolute flex justify-start transform -translate-y-1/2 w-2/5 left-24 mt-16 lg:mt-40 top-1/4">
                 <Link to='/contact'>
-                    <button className="p-2 lg:p-3 mt-24 rounded-lg font-semibold bg-orange-500 text-black mr-5">Contact Me</button>
+                    <button className="p-2 lg:p-3 mt-24 rounded-lg font-semibold text-white bg-orange-500 mr-5">Contact Me</button>
                 </Link>
             </div>
         </div>
